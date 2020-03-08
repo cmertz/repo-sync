a83a0d193eddff577ad665ef40cb3af3f8efba67
Christoph Mertz <cmertz@users.noreply.github.com>
2020-03-08 17:11:55 +0100

    extract proper repo type

--- main.go
+++ main.go
@@ -21,80 +21,52 @@ import (
 )
 
 const (
-	kindGitlab    = "gitlab"
-	kindGitea     = "gitea"
-	kindGithub    = "github"
-	kindPlainRepo = "git"
+	kindGitlab  = "gitlab"
+	kindGitea   = "gitea"
+	kindGithub  = "github"
+	kindGitRepo = "git"
 )
 
-type repoURL string
-
-func (r repoURL) path(prefix string) string {
-	u := string(r)
-	if strings.HasPrefix(u, "git@") {
-		u = strings.TrimPrefix(u, "git@")
-	}
-
-	if strings.HasSuffix(u, ".git") {
-		u = strings.TrimSuffix(u, ".git")
-	}
-
-	return prefix + "/" + strings.Replace(u, ":", "/", -1)
+type repo struct {
+	remote string
+	path   string
 }
 
-func (r repoURL) ensureParentDirsExist(prefix string) {
-	parts := strings.Split(r.path(prefix), "/")
+func (r repo) sync(ctx context.Context) error {
+	parts := strings.Split(r.path, "/")
 	parentDir := strings.Join(parts[:len(parts)-1], "/")
 
-	if err := os.MkdirAll(parentDir, 0700); err != nil {
-		panic(err)
-	}
-}
-
-func (r repoURL) clone(ctx context.Context, prefix string) error {
-	r.ensureParentDirsExist(prefix)
-
-	_, err := git.PlainCloneContext(ctx, r.path(prefix), false, &git.CloneOptions{
-		URL: string(r),
-	})
-
-	if err == transport.ErrEmptyRemoteRepository {
-		return nil
-	}
-
-	return err
-}
-
-func (r repoURL) pull(ctx context.Context, prefix string) error {
-	gitRepo, err := git.PlainOpen(r.path(prefix))
+	err := os.MkdirAll(parentDir, 0700)
 	if err != nil {
 		return err
 	}
 
+	repo, err := git.PlainOpen(r.path)
+	if err != nil {
+		repo, err = git.PlainCloneContext(ctx, r.path, false, &git.CloneOptions{
+			URL: r.remote,
+		})
+
+		// ignore empty remotes
+		if err != nil && err != transport.ErrEmptyRemoteRepository {
+			return err
+		}
+	}
+
 	var w *git.Worktree
-	w, err = gitRepo.Worktree()
+	w, err = repo.Worktree()
 	if err != nil {
 		return err
 	}
 
 	err = w.PullContext(ctx, &git.PullOptions{RemoteName: "origin"})
+
+	// ignore if there's nothing to update
 	if err == git.NoErrAlreadyUpToDate {
 		return nil
 	}
+
 	return err
-}
-
-func (r repoURL) hasLocalCopy(prefix string) bool {
-	_, err := git.PlainOpen(r.path(prefix))
-	return err == nil
-}
-
-func (r repoURL) sync(ctx context.Context, prefix string) error {
-	if !r.hasLocalCopy(prefix) {
-		return r.clone(ctx, prefix)
-	}
-
-	return r.pull(ctx, prefix)
 }
 
 type repoSource struct {
@@ -102,6 +74,10 @@ type repoSource struct {
 	URL          string
 	Username     string
 	TokenCommand string `yaml:"tokenCommand"`
+
+	// either one of those needs to be provided
+	Prefix string
+	Path   string
 }
 
 func (r repoSource) token(ctx context.Context) (string, error) {
@@ -118,7 +94,7 @@ func (r repoSource) token(ctx context.Context) (string, error) {
 	return strings.TrimSpace(out.String()), nil
 }
 
-func (r repoSource) repos(ctx context.Context) ([]repoURL, error) {
+func (r repoSource) repos(ctx context.Context) ([]repo, error) {
 	token, err := r.token(ctx)
 	if err != nil {
 		return nil, err
@@ -126,19 +102,31 @@ func (r repoSource) repos(ctx context.Context) ([]repoURL, error) {
 
 	switch r.Kind {
 	case kindGitlab:
-		return reposGitlab(ctx, r.URL, token)
+		return reposGitlab(ctx, r.URL, token, r.Prefix)
 	case kindGithub:
-		return reposGithub(ctx, token, r.Username)
+		return reposGithub(ctx, token, r.Username, r.Prefix)
 	case kindGitea:
-		return reposGitea(r.URL, token, r.Username)
-	case kindPlainRepo:
-		return []repoURL{repoURL(r.URL)}, nil
+		return reposGitea(r.URL, token, r.Username, r.Prefix)
+	case kindGitRepo:
+		return []repo{repo{remote: r.URL, path: r.Path}}, nil
 	default:
 		panic(fmt.Sprintf("unknown provider kind '%s'", r.Kind))
 	}
 }
 
-func reposGitea(url, token, username string) ([]repoURL, error) {
+func path(prefix, url string) string {
+	if strings.HasPrefix(url, "git@") {
+		url = strings.TrimPrefix(url, "git@")
+	}
+
+	if strings.HasSuffix(url, ".git") {
+		url = strings.TrimSuffix(url, ".git")
+	}
+
+	return prefix + "/" + strings.Replace(url, ":", "/", -1)
+}
+
+func reposGitea(url, token, username, prefix string) ([]repo, error) {
 	c := gitea.NewClient(url, token)
 
 	repos, err := c.ListMyRepos()
@@ -146,14 +134,17 @@ func reposGitea(url, token, username string) ([]repoURL, error) {
 		return nil, err
 	}
 
-	var res []repoURL
-	for _, repo := range repos {
-		res = append(res, repoURL(repo.SSHURL))
+	var res []repo
+	for _, r := range repos {
+		res = append(res, repo{
+			remote: r.SSHURL,
+			path:   path(prefix, r.SSHURL),
+		})
 	}
 	return res, nil
 }
 
-func reposGithub(ctx context.Context, token, username string) ([]repoURL, error) {
+func reposGithub(ctx context.Context, token, username, prefix string) ([]repo, error) {
 	httpClient := oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token}))
 	client := githubv4.NewClient(httpClient)
 
@@ -179,15 +170,19 @@ func reposGithub(ctx context.Context, token, username string) ([]repoURL, error)
 		return nil, err
 	}
 
-	var res []repoURL
+	var res []repo
 	for _, edge := range query.Search.Edges {
-		res = append(res, repoURL(edge.Node.Repository.SSHURL))
+		repoURL := string(edge.Node.Repository.SSHURL)
+		res = append(res, repo{
+			remote: repoURL,
+			path:   path(prefix, repoURL),
+		})
 	}
 
 	return res, nil
 }
 
-func reposGitlab(ctx context.Context, url, token string) ([]repoURL, error) {
+func reposGitlab(ctx context.Context, url, token, prefix string) ([]repo, error) {
 	c := gitlab.NewClient(nil, token)
 	c.SetBaseURL(url)
 
@@ -196,35 +191,15 @@ func reposGitlab(ctx context.Context, url, token string) ([]repoURL, error) {
 		return nil, err
 	}
 
-	var res []repoURL
-	for _, repo := range repos {
-		res = append(res, repoURL(repo.SSHURLToRepo))
+	var res []repo
+	for _, r := range repos {
+		res = append(res, repo{
+			remote: r.SSHURLToRepo,
+			path:   path(prefix, r.SSHURLToRepo),
+		})
 	}
 
 	return res, nil
-}
-
-type repoSources struct {
-	Sources []repoSource
-	Prefix  string
-}
-
-func (r repoSources) sync(ctx context.Context) error {
-	for _, s := range r.Sources {
-		repos, err := s.repos(ctx)
-		if err != nil {
-			return err
-		}
-
-		for _, repo := range repos {
-			err = repo.sync(ctx, r.Prefix)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
 }
 
 func main() {
@@ -239,8 +214,8 @@ func main() {
 		panic(err)
 	}
 
-	var cfg repoSources
-	err = yaml.Unmarshal(b, &cfg)
+	var sources []repoSource
+	err = yaml.Unmarshal(b, &sources)
 	if err != nil {
 		panic(err)
 	}
@@ -256,8 +231,17 @@ func main() {
 		}
 	}()
 
-	err = cfg.sync(ctx)
-	if err != nil {
-		panic(err)
+	for _, s := range sources {
+		repos, err := s.repos(ctx)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, repo := range repos {
+			err = repo.sync(ctx)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
 }
