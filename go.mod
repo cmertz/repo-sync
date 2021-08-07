9538271a126387cc32ca375b662c8984503e8543
Christoph Mertz <cmertz@users.noreply.github.com>
2021-08-07 16:57:16 +0200

    add gitlab source for remote urls to sync from

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/shurcooL/githubv4 v0.0.0-20210725200734-83ba7b4c9228
 	github.com/shurcooL/graphql v0.0.0-20200928012149-18c5c3165e3a // indirect
+	github.com/xanzy/go-gitlab v0.50.2
 	golang.org/x/oauth2 v0.0.0-20210805134026-6f1e6394065a
 )
---
+++ source/gitlab.go
@@ -0,0 +1,43 @@
+package source
+
+import (
+	"context"
+
+	"github.com/xanzy/go-gitlab"
+)
+
+// Gitlab is a gitlab source for remotes to sync from
+type Gitlab struct {
+	URL   string
+	Token string
+}
+
+// List remote repository urls
+func (g Gitlab) List(ctx context.Context) ([]string, error) {
+	client, err := gitlab.NewClient(
+		g.Token,
+		gitlab.WithBaseURL(g.URL),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: pagination
+	repos, resp, err := client.Projects.ListProjects(
+		&gitlab.ListProjectsOptions{
+			Visibility: gitlab.Visibility(gitlab.PrivateVisibility),
+		},
+		gitlab.WithContext(ctx),
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var res []string
+	for _, r := range repos {
+		res = append(res, r.SSHURLToRepo)
+	}
+
+	return res, nil
+}
