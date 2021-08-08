ba07dc57a7158cd0a9c305c83e0515b1740ffff6
Christoph Mertz <cmertz@users.noreply.github.com>
2021-08-08 11:27:49 +0200

    sync/remote: refine generation of local paths from remote url

--- sync/remote.go
+++ sync/remote.go
@@ -8,15 +8,24 @@ func (r remote) url() string {
 	return string(r)
 }
 
-func (r remote) basepath() string {
-	path := strings.TrimSuffix(r.url(), ".git")
+func (r remote) local(prefix string) local {
+	p := strings.TrimSuffix(r.url(), ".git")
 
 	for _, prefix := range []string{"git@", "git://", "ssh://", "https://", "http://"} {
-		if strings.HasPrefix(path, prefix) {
-			path = strings.TrimPrefix(path, prefix)
+
+		// strip only the first prefix we find, everything else is odd
+		if strings.HasPrefix(p, prefix) {
+			p = strings.TrimPrefix(p, prefix)
+
 			break
 		}
 	}
 
-	return strings.ReplaceAll(path, ":", "/")
+	p = strings.ReplaceAll(p, ":", "/")
+
+	if len(prefix) != 0 {
+		p = prefix + "/" + p
+	}
+
+	return local(p)
 }
---
+++ sync/remote_test.go
@@ -0,0 +1,61 @@
+package sync
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestRemote_local(t *testing.T) {
+	cases := []struct {
+		remote string
+		prefix string
+		local  string
+	}{
+		{
+			"example.com",
+			"",
+			"example.com",
+		},
+		{
+			"example.com",
+			"src",
+			"src/example.com",
+		},
+		{
+			"git@example.com:user/1",
+			"src",
+			"src/example.com/user/1",
+		},
+		{
+			"ssh://example.com/user/2",
+			"src",
+			"src/example.com/user/2",
+		},
+		{
+			"git://example.com/user/3",
+			"src",
+			"src/example.com/user/3",
+		},
+		{
+			"https://example.com/user/4",
+			"src",
+			"src/example.com/user/4",
+		},
+		{
+			"http://example.com/user/5",
+			"src",
+			"src/example.com/user/5",
+		},
+	}
+
+	for i, c := range cases {
+		c := c
+
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			l := string(remote(c.remote).local(c.prefix))
+			if l != c.local {
+				t.Errorf("expected %s, actual %s", c.local, l)
+			}
+		})
+	}
+}
